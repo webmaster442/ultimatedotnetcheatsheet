d82edc2ee85bfa3584edb4deb4799185973f9ddf
webmaster442 <ruzsinszki.gabor@gmail.com>
2023-08-01 21:20:47 +0200

    build tools finished

--- build.sh
+++ build.sh
@@ -33,4 +33,4 @@ fi
 
 mkdir output
 pandoc --self-contained --toc --metadata pagetitle=".NET Cheet sheet" --template template/template.html -c template/mvp.css -c template/style.css -i cheatsheet.md -o output/build.html
-wkhtmltopdf --page-size A4 output/build.html output/build.pdf
+wkhtmltopdf --page-size A4 --image-quality 99 -B 10 -L 10 -R 10 -T 10 output/build.html output/build.pdf
