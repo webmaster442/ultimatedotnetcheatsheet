b53cf19fadbd9d509b2449fa8494e5b0910ccb0a
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2024-02-09 19:14:25 +0100

    Work on rendering

--- build.ps1
+++ build.ps1
@@ -1,1 +1,6 @@
-bookgen md2html --svg --title "Ultimate .NET Cheat sheet" -i .\cheatsheet.md --css style.css -o output\cheatsheet.html
+$f1 = Get-Content -Path .\cheatsheet.md
+$f2 = Get-Content -Path .\footer.html
+$merged = $f1 + $f2
+$merged | Out-File -FilePath .\torender.md
+bookgen md2html --svg --title "Ultimate .NET Cheat sheet" -i .\torender.md --css style.css -o output\index.html
+Remove-Item -Path .\torender.md
