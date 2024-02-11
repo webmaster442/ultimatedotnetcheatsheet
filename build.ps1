68f7f90d3408a3b9881e6c80c08169028fdce41f
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2024-02-11 13:02:11 +0100

    Favicon

--- build.ps1
+++ build.ps1
@@ -2,6 +2,6 @@ $f1 = Get-Content -Path .\cheatsheet.md
 $f2 = Get-Content -Path .\footer.html
 $merged = $f1 + $f2
 $merged | Out-File -FilePath .\torender.md
-bookgen md2html --svg --title "Ultimate .NET Cheat sheet" -i .\torender.md --css style.css -o gh-pages\index.html
+bookgen md2html --svg --title "Ultimate .NET Cheat sheet" -i .\torender.md --css style.css -o docs\index.html
 Remove-Item -Path .\torender.md
-tidy -m gh-pages\index.html
+tidy -m docs\index.html
