1beedf1c55467246a8e862bd72ea2cd10dfe6355
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2024-02-11 14:19:32 +0100

    Transition to new bookgen

--- build.ps1
+++ build.ps1
@@ -1,7 +1,2 @@
-$f1 = Get-Content -Path .\cheatsheet.md
-$f2 = Get-Content -Path .\footer.html
-$merged = $f1 + $f2
-$merged | Out-File -FilePath .\torender.md
-bookgen md2html --svg --title "Ultimate .NET Cheat sheet" -i .\torender.md --css style.css -o docs\index.html
-Remove-Item -Path .\torender.md
-tidy -m docs\index.html
+bookgen md2html --svg --title "Ultimate .NET Cheat sheet" -i .\cheatsheet.md --no-css -tf .\template.html -o docs\index.html
+tidy -i -m -ashtml docs\index.html
