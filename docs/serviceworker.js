56c5dc2ecd204e214ed7ef297f14673d8992a1e2
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2024-04-01 15:24:24 +0200

    Better service worker

--- docs/serviceworker.js
+++ docs/serviceworker.js
@@ -1,36 +1,69 @@
-const CACHE_NAME = `ultimate-net-cheatsheet-v1`;
+// Define the cache name and the files to cache
+var CACHE_NAME = 'ultimate-dotnet-cheatsheet-cache-v1';
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/script.js',
+  '/style.css'
+];
 
-// Use the install event to pre-cache all initial resources.
-self.addEventListener('install', event => {
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    cache.addAll([
-      '/',
-      '/style.css',
-      '/script.js'
-    ]);
-  })());
+// Install the service worker and cache the assets
+self.addEventListener('install', function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith((async () => {
-    const cache = await caches.open(CACHE_NAME);
-
-    // Get the resource from the cache.
-    const cachedResponse = await cache.match(event.request);
-    if (cachedResponse) {
-      return cachedResponse;
-    } else {
-        try {
-          // If the resource was not in the cache, try the network.
-          const fetchResponse = await fetch(event.request);
-
-          // Save the resource in the cache and return it.
-          cache.put(event.request, fetchResponse.clone());
-          return fetchResponse;
-        } catch (e) {
-          // The network failed.
+// Serve cached content when offline
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
         }
-    }
-  })());
+
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function (response) {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function (cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
+
+// Clean up old caches
+self.addEventListener('activate', function (event) {
+  var cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
